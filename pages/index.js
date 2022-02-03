120450ae95e354621966a54bb55e86f6407c3b65
meloncollieyo <68175873+meloncollieyo@users.noreply.github.com>
2022-02-02 19:01:39 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="ayyyye it's hau" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          holla at yo girl <a href="mailto:hello@hautran.com">here</a>
         </p>
       </main>
 
